c663533db898836c8029331f02ff0690959d804f
Intregra <gtpaul@seznam.cz>
2018-04-13 12:35:06 +0200

    added translation for feedback form; fixed overlaping of feedback button with srcoll_to_top arrow

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <?php if (FEEDBACK > 0) { ?>
 	<!-- feedback area -->
-	<button class="feedback_button" data-toggle="modal" data-target="#modal_feedback">Feedback</button>
+	<button class="feedback_button" data-toggle="modal" data-target="#modal_feedback"><?php echo $GLOBALS['lang']->other->feedbackTitle; ?></button>
 	<div id="modal_feedback" class="modal fade" role="dialog">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<button type="button" class="modal-close-button" data-dismiss="modal">x</button>
-				<div class="subject_header">Váš podnět</div>
+				<div class="subject_header"><?php echo $GLOBALS['lang']->other->feedbackDesc; ?></div>
 				<textarea id="feedback_textarea" rows="3"></textarea>
-				<button type="button" class="feedback_submit">Odeslat</button>
+				<button type="button" class="feedback_submit"><?php echo $GLOBALS['lang']->other->send; ?></button>
 			</div>
 		</div>
 	</div>
